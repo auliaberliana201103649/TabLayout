6c24df1d8aea1f6eb5b7fb0aae8589d449afec0a
AuliaBerliana201103649 <auliaaberliana@gmail.com>
2023-05-05 14:40:48 +0700

    tablayout

---
+++ app/src/main/java/com/auliaberliana201103649/tablayout/MainActivity.java
@@ -0,0 +1,14 @@
+package com.auliaberliana201103649.tablayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
